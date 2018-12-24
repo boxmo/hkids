b3cbf5f9e5ea566fa9434c2a5b355998fe35ec66
boxmo <mztbrum@gmail.com>
2018-12-24 17:52:49 -0200

    responsive

--- js/script_maes.js
+++ js/script_maes.js
@@ -10,8 +10,31 @@ $(document).ready(function(){
     slidesToShow: 3,
     slidesToScroll: 3,
     speed: 800,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
-
+  
   slider.on('wheel', (function(e) {
     e.preventDefault();
     console.log(e);
--- js/script_meninas.js
+++ js/script_meninas.js
@@ -91,7 +91,30 @@ $(document).ready(function(){
     lazyLoad: 'ondemand',
     slidesToShow: 3,
     slidesToScroll: 3,
-    speed: 800
+    speed: 800,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   slider.on('wheel', (function(e) {
